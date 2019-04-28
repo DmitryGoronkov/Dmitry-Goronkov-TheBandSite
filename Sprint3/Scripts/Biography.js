0ed490912fc95c2f1978740805badbe47354801a
Dmitry Goronkov <gordima@yahoo.com>
2019-04-28 12:13:10 -0400

    Final commit. Delete button works without refreshing the page and is added to only new posted comments

--- Sprint3/Scripts/Biography.js
+++ Sprint3/Scripts/Biography.js
@@ -18,41 +18,36 @@ function timestampConversion(timestamp){
 }
 
 // The function below adds a comment on the page
-function displayComment(singleComment){
-        const block = document.querySelector(".commentsec__existing__one");
-        let newblock = block.cloneNode(true);
-        const selector = ".commentsec__existing__one__comment";
-        newblock.querySelector(`${selector}__header--name`).innerHTML = singleComment.name;
-        newblock.querySelector(`${selector}__header--date`).innerHTML = singleComment.date;
-        // The code below add images to different users
-        const photoSelector = ".commentsec__existing__one__image";
-        function imageAdd (url){
-            newblock.querySelector(`${photoSelector}`).style.backgroundImage = `${url}`;
-            newblock.querySelector(`${photoSelector}`).style.backgroundPosition = "center";
-            newblock.querySelector(`${photoSelector}`).style.backgroundSize = "cover";
-        }
-        if (singleComment.photo){
-            imageAdd(`url("./Assets/Images/Mohan-muruge.jpg")`);
-            newblock.querySelector(`.commentsec__existing__one__comment__button`).style.display = "block";
-            
-            
-            
-        }
-        if (singleComment.name === "Micheal Lyons"){
-            imageAdd(`url("./Assets/Images/Michael.jpg")`);
-        }
-        if (singleComment.name === "Gary Wong"){
-            imageAdd(`url("./Assets/Images/Gary.jpg")`);
-        }
-        if (singleComment.name === "Theodore Duncan"){
-            imageAdd(`url("./Assets/Images/Teo.jpg")`);
-        }
-        newblock.querySelector(`${selector}__body`).innerHTML = singleComment.comment;
-        newblock.querySelector(`${selector}__body`).overflowWrap = "break-word";
-        newblock.querySelector(`.commentsec__existing__one__comment__button`).id = singleComment.id;
-        
-        document.querySelector(".commentsec__existing").prepend(newblock);
-        
+function displayComment(singleComment) {
+    const block = document.querySelector(".commentsec__existing__one");
+    let newblock = block.cloneNode(true);
+    const selector = ".commentsec__existing__one__comment";
+    newblock.querySelector(`${selector}__header--name`).innerHTML = singleComment.name;
+    newblock.querySelector(`${selector}__header--date`).innerHTML = singleComment.date;
+    // The code below add images to different users
+    const photoSelector = ".commentsec__existing__one__image";
+    function imageAdd(url) {
+        newblock.querySelector(`${photoSelector}`).style.backgroundImage = `${url}`;
+        newblock.querySelector(`${photoSelector}`).style.backgroundPosition = "center";
+        newblock.querySelector(`${photoSelector}`).style.backgroundSize = "cover";
+    }
+    if (singleComment.photo) {
+        imageAdd(`url("./Assets/Images/Mohan-muruge.jpg")`);
+        newblock.querySelector(`.commentsec__existing__one__comment__button`).style.display = "block";
+    }
+    if (singleComment.name === "Micheal Lyons") {
+        imageAdd(`url("./Assets/Images/Michael.jpg")`);
+    }
+    if (singleComment.name === "Gary Wong") {
+        imageAdd(`url("./Assets/Images/Gary.jpg")`);
+    }
+    if (singleComment.name === "Theodore Duncan") {
+        imageAdd(`url("./Assets/Images/Teo.jpg")`);
+    }
+    newblock.querySelector(`${selector}__body`).innerHTML = singleComment.comment;
+    newblock.querySelector(`${selector}__body`).overflowWrap = "break-word";
+    newblock.querySelector(`.commentsec__existing__one__comment__button`).id = singleComment.id;
+    document.querySelector(".commentsec__existing").prepend(newblock);
 }
 
 //Function below gets the comments from API
@@ -79,26 +74,39 @@ function getComments(){
         }
         comList.forEach(comment => {
             displayComment(comment);
-            function deleteComment (){
-                console.log(comment.id);
-                axios.delete(`https://project-1-api.herokuapp.com/comments/${comment.id}?api_key=${ApiKey}`, {})
-                .then (response => {
-                    console.log(response.data);
-                    location.reload();
-                })
-            }
-            document.getElementById(`${comment.id}`).addEventListener("click", deleteComment);
         });
-        console.log(comList);
-        
-        
-        
-        
+        deleteAndGet();
     })
+    .catch(error => {
+        console.log("Could not connect to the server to get the comment data");
+    });
 }
 getComments();
 
+///// delete comment function. Delete button is added only to new posted comments 
+function deleteAndGet() {
+    comList.forEach(comment => {
+        function deleteComment (){
+            axios.delete(`https://project-1-api.herokuapp.com/comments/${comment.id}?api_key=${ApiKey}`, {})
+            .then (response => {
+                let comListObjs = document.querySelector(".commentsec__existing");
+                while (comListObjs.firstChild) { 
+                    comListObjs.removeChild(comListObjs.firstChild);
+                }
+                comList = [];
+                getComments();
+            })
+            .catch(error => {
+                console.log("Could not connect to the server to delete the comment data");
+            });
+        }
+        document.getElementById(`${comment.id}`).addEventListener("click", deleteComment);
+    });
+}
+
+
 // function below adds a new comment to the array, clears the comments list and rebuilds it again with a new updated array.
+
 function addComment(event) {
     event.preventDefault();
     let newComment = document.getElementById("comment").value;
@@ -117,45 +125,11 @@ function addComment(event) {
         getComments();
         
     })
-    
+    .catch(error => {
+        console.log("Could not connect to the server to post the comment data");
+    });
 }
 
 //The following code adds a function to the click of the comment button
 var commentForm = document.querySelector(".commentsec__inputsec__form");
 commentForm.addEventListener("submit", addComment);
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-// /////////////////
-// axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${ApiKey}`)
-//     .then (response => {
-//         console.log(response.data);
-//     })
-
-
-
-// axios.delete(`https://project-1-api.herokuapp.com/comments/8de500a7-9986-47fa-80a5-99e0ff3dd845?api_key=${ApiKey}`, {})
-//     .then (response => {
-//         console.log(response.data);
-// })
-// axios.delete(`https://project-1-api.herokuapp.com/comments/d6dfb910-71e8-4628-9f9f-f86f07692299?api_key=${ApiKey}`, {})
-//     .then (response => {
-//         console.log(response.data);
-// })
-
-// axios.delete(`https://project-1-api.herokuapp.com/comments/3d269cbe-5388-4910-a2d4-56180f44b56b?api_key=${ApiKey}`, {})
-//     .then (response => {
-//         console.log(response.data);
-// })
