6b56fd1cc57aa83eddb62750f59bb7932aaa589e
Dmitry Goronkov <gordima@yahoo.com>
2019-04-27 23:19:48 -0400

    Delete button is added to every new comment. The page reload with a delete. Try to fix that

--- Sprint3/Scripts/Biography.js
+++ Sprint3/Scripts/Biography.js
@@ -33,6 +33,10 @@ function displayComment(singleComment){
         }
         if (singleComment.photo){
             imageAdd(`url("./Assets/Images/Mohan-muruge.jpg")`);
+            newblock.querySelector(`.commentsec__existing__one__comment__button`).style.display = "block";
+            
+            
+            
         }
         if (singleComment.name === "Micheal Lyons"){
             imageAdd(`url("./Assets/Images/Michael.jpg")`);
@@ -45,8 +49,10 @@ function displayComment(singleComment){
         }
         newblock.querySelector(`${selector}__body`).innerHTML = singleComment.comment;
         newblock.querySelector(`${selector}__body`).overflowWrap = "break-word";
-        newblock.style.display = block;
+        newblock.querySelector(`.commentsec__existing__one__comment__button`).id = singleComment.id;
+        
         document.querySelector(".commentsec__existing").prepend(newblock);
+        
 }
 
 //Function below gets the comments from API
@@ -57,10 +63,12 @@ function getComments(){
             let newComment = response.data[i].comment;
             let newName = response.data[i].name;
             let newDate = timestampConversion(response.data[i].timestamp);
+            let id = response.data[i].id;
             let newComObj = {
                 name: newName,
                 comment: newComment,
-                date: newDate
+                date: newDate,
+                id: id
             };
             // The conditional statement below will just add a visual feature of having Mohan's photo added to every new comment. Approved by Ed Jackson :)
             if (response.data[i].timestamp > 1556396118896){
@@ -71,8 +79,21 @@ function getComments(){
         }
         comList.forEach(comment => {
             displayComment(comment);
+            function deleteComment (){
+                console.log(comment.id);
+                axios.delete(`https://project-1-api.herokuapp.com/comments/${comment.id}?api_key=${ApiKey}`, {})
+                .then (response => {
+                    console.log(response.data);
+                    location.reload();
+                })
+            }
+            document.getElementById(`${comment.id}`).addEventListener("click", deleteComment);
         });
         console.log(comList);
+        
+        
+        
+        
     })
 }
 getComments();
@@ -92,41 +113,44 @@ function addComment(event) {
             comListObjs.removeChild(comListObjs.firstChild);
         }
         comList = [];
+        
         getComments();
+        
     })
+    
 }
+
 //The following code adds a function to the click of the comment button
 var commentForm = document.querySelector(".commentsec__inputsec__form");
 commentForm.addEventListener("submit", addComment);
 
 
 
+ 
 
 
 
 
 
 
-/////////////////
-axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${ApiKey}`)
-    .then (response => {
-        console.log(response.data);
-    })
 
-// axios.delete(`https://project-1-api.herokuapp.com/comments/254a21b5-121b-453f-b493-ceb1d9bd0b55?api_key=${ApiKey}`, {})
+
+
+
+
+// /////////////////
+// axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${ApiKey}`)
+//     .then (response => {
+//         console.log(response.data);
+//     })
+
+
+
+// axios.delete(`https://project-1-api.herokuapp.com/comments/8de500a7-9986-47fa-80a5-99e0ff3dd845?api_key=${ApiKey}`, {})
 //     .then (response => {
 //         console.log(response.data);
 // })
-// axios.delete(`https://project-1-api.herokuapp.com/comments/91f6a798-512c-4bee-ad27-89916f9ea44c?api_key=${ApiKey}`, {})
-//     .then (response => {
-//         console.log(response.data);
-// })
-
-// axios.delete(`https://project-1-api.herokuapp.com/comments/42a33647-a89e-4087-b24f-7a3766259e5a?api_key=${ApiKey}`, {})
-//     .then (response => {
-//         console.log(response.data);
-// })
-// axios.delete(`https://project-1-api.herokuapp.com/comments/a18d00c7-a270-49a7-bdbf-0658b016c1d7?api_key=${ApiKey}`, {})
+// axios.delete(`https://project-1-api.herokuapp.com/comments/d6dfb910-71e8-4628-9f9f-f86f07692299?api_key=${ApiKey}`, {})
 //     .then (response => {
 //         console.log(response.data);
 // })
